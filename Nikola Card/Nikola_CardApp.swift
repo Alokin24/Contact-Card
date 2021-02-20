d236a7dac807caec8ded82fcfd9726f53d35136d
Nikola Anastasovski <nikola.anastasovski@netcetera.com>
2021-02-20 12:21:32 +0100

    Initial Commit

---
+++ Nikola Card/Nikola_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nikola_CardApp.swift
+//  Nikola Card
+//
+//  Created by Nikola Anastasovski on 20.2.21.
+//
+
+import SwiftUI
+
+@main
+struct Nikola_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
